f8c81561ea70b6c152011bda03d5fcc451a94f38
Passionatedreamer <passionatedreamer19@gmail.com>
2017-08-09 04:25:46 +0500

    Fixing permissions response

--- src/Middleware/CheckPermissionsMiddleware.php
+++ src/Middleware/CheckPermissionsMiddleware.php
@@ -114,15 +114,12 @@ class CheckPermissionsMiddleware
             }
         }
         if ($permission && (!$request->user() || !$request->user()->can($permission))){
-            if ($request->ajax()) {
-                $error = ['error' => ["You are not authorized to view this content!"]];
-                return response($error, 401); 
-            }
-
-            return abort(401, 'You are not authorized to view this content!');
+            $error = ['error' => ["You are not authorized to view this content!"]];
+            return response($error, 401); 
         }
-
-        return $next($request);
     }
+
+    return $next($request);
+}
 
 }
